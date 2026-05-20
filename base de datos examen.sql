67e5ac39998bf4fed841b21d1e43803337ed3752
Jostyn <tuemail@example.com>
2026-05-20 11:19:34 -0600

    first commit

---
+++ base de datos examen.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE restaurante_bd;
+
+USE restaurante_bd;
+
+CREATE TABLE platillos(
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   nombre VARCHAR(100) NOT NULL
+   );
+   
+   select * from platillos;
+   
+   
